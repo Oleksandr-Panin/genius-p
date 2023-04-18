e2c095007c6af0aae0d18b7827f5baad1c3e33ef
Oleksandr-Panin <130825827+Oleksandr-Panin@users.noreply.github.com>
2023-04-18 22:49:22 +0200

    home

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-//-----------
-
-/* hygj*/
-
-//console.log(a); //ReferenceError: a is n ot defined
-//let a = 10;
-//console.log(a);
-//a = 20;
-//console.log(a); //20
-
-//console.log(b); ReferenceError: b is not defined
-//const b = 15;
-//console.log(b); //15
-
-console.log(1 + 2)
+let name = "ivan":
+let city = "Kyiv":
+city = name;
+console.log(city);
